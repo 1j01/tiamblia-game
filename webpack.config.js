b4279ca14e6c427620cb2d3ec4d52c927ad571a4
Isaiah Odhner <isaiahodhner@gmail.com>
2023-02-16 18:51:46 -0500

    Update dependencies and webpack config
    This is going about a million times smoother because I have already
    figured it out for the Skele2D example, and am copying from there,
    but also because I'm not dealing with a second webpack setup for the
    Skele2D library itself, and UglifyJS incompatibility, and other things.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-
-const IS_NW = false; //TODO
+// const TerserPlugin = require("terser-webpack-plugin");
 
 const config = {
   context: path.join(__dirname, 'src'),
@@ -11,6 +10,12 @@ const config = {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/build/',
+    hashFunction: 'xxhash64',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, ""),
+    },
   },
   module: {
     rules: [
@@ -22,25 +27,29 @@ const config = {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
       },
-    ]
+    ],
   },
-  // TODO: web? specify build target somewhere?
-  node: {
-    fs: 'empty'
-  },
+  // I've lost some context for what I was doing here.
+  // TODO: is this helping? I should test NW.js auto-saving the world, animations/poses.
+  // Actually, I should ditch NW.js and just use the FS Access API.
+  // I didn't care about NW.js for distribution, right? Just for development.
+  // node: {
+  //   fs: 'empty'
+  // },
   // resolve: {
   //   alias: {
   //     fs: IS_NW ? 'fs' : 'empty-module',
   //     path: IS_NW ? 'path' : 'empty-module',
   //   }
   // },
-
-  // avoiding name-mangling for entity classes
-  // TODO: re-enable minification, without name mangling
-  // or avoid the specific name mangling somehow,
-  // or stop using function.name
+  // TODO: re-enable minification (prod only), now that I've figured out how avoid name mangling
   optimization: {
     minimize: false,
+    // minimizer: [new TerserPlugin({
+    //   terserOptions: {
+    //     keep_classnames: true, // needed for serialization, and display in the entities bar UI
+    //   },
+    // })],
   },
 };
 module.exports = config;
