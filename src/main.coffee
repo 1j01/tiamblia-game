1j01/tiamblia-game

commit ac5d8fe74c669af84ea2ab8a224e839b82e0bf65

Fix collision response at seams by separating concerns
- Polygon splitting is for collision detection, to fit in buckets,
  and to reduce polygon sizes for the point-in-polygon algorithm.
  The original polygons are better for collision response and rendering.
  (Even better for collision response would be further-joined polygons,
  where they overlap, while keeping the original polygons for rendering,
  so it would be three tier system in that case.)
- This ensures derived polygons are never saved into the world data.
- This fixes caterpillars tunneling into seams.

src/main.coffee
@@ -109,7 +109,12 @@ redraw = ->
 				ctx.translate(entity.x, entity.y)
 				ctx.strokeStyle = if entity.solid is false then "blue" else "red"
 				ctx.fillStyle = if entity.solid is false then "rgba(0, 0, 255, 0.2)" else "rgba(255, 0, 0, 0.2)"
-				ctx.lineWidth = 1 / view.scale
+				if entity.intangible
+					ctx.setLineDash([5, 5])
+					ctx.fillStyle = "transparent"
+					ctx.lineWidth = 4 / view.scale
+				else
+					ctx.lineWidth = 1 / view.scale
 				ctx.beginPath()
 				points = Object.values(entity.structure.points)
 				ctx.moveTo(points[0].x, points[0].y)
@@ -119,6 +124,21 @@ redraw = ->
 				ctx.stroke()
 				ctx.fill()
 				ctx.translate(-entity.x, -entity.y)
+				ctx.setLineDash([])
+		for entity in world.derived_colliders
+			ctx.translate(entity.x, entity.y)
+			ctx.strokeStyle = if entity.solid is false then "aqua" else "fuchsia"
+			ctx.fillStyle = if entity.solid is false then "rgba(0, 255, 255, 0.2)" else "rgba(255, 0, 255, 0.2)"
+			ctx.lineWidth = 1 / view.scale
+			ctx.beginPath()
+			points = Object.values(entity.structure.points)
+			ctx.moveTo(points[0].x, points[0].y)
+			for point in points
+				ctx.lineTo(point.x, point.y)
+			ctx.closePath()
+			ctx.stroke()
+			ctx.fill()
+			ctx.translate(-entity.x, -entity.y)
 	
 	show_collision_buckets = (try localStorage["tiamblia.show_collision_buckets"]) is "true"
 	if show_collision_buckets

src/World.coffee
@@ -19,6 +19,7 @@ closestPointOnLineSegment = (point, a, b)->
 module.exports = class World
 	constructor: ->
 		@entities = []
+		@derived_colliders = []
 	
 	@format: "Tiamblia World"
 	@formatVersion: 4
@@ -210,7 +211,7 @@ module.exports = class World
 	bucket_height = 100
 	updateCollisionBuckets: ->
 		@collision_buckets = {}
-		for entity in @entities
+		for entity in [...@entities, ...@derived_colliders] when not entity.intangible
 			# For PolygonStructure, we can use the bounding box of the structure
 			# which ignores bbox_padding which shouldn't apply to polygons.
 			# For BoneStructure, bbox_padding makes lineThickness in collision detection work
@@ -268,11 +269,32 @@ module.exports = class World
 		# Divides terrain into smaller polygons horizontally,
 		# so it can fit into the collision buckets.
 		# This happens at the start of the game.
+		# Instead of creating new entities of the same type as the original,
+		# we'll leave the original entity for visual purposes,
+		# and store the derived colliders separately from entities, without serialization.
+		# This strategy allows perfect visual fidelity without hacks,
+		# and prevents the possibility of losing the original polygons
+		# by saving after starting the simulation, or doubly optimizing, creating slivers.
+
+		# First, undo any previous optimization.
+		# TODO: rename "old" -> "original"/"source"/"base"/"parent"/something,
+		# since it's no longer replaced entirely.
+		@derived_colliders = []
 		old_terrain_entities = @getEntitiesOfType(Terrain)
-		# Exempt water because 1. it's transparent, so overlapping polygons will not avoid rendering artifacts,
+		for old_terrain_entity in old_terrain_entities
+			if old_terrain_entity.intangible_because_optimized
+				delete old_terrain_entity.intangible
+				delete old_terrain_entity.intangible_because_optimized
+		
+		# I was exempting water because 1. it's transparent, so overlapping polygons will not avoid rendering artifacts,
 		# and 2. it renders reflections using drawImage, which is expensive,
 		# and 3. it's not solid, so some collision checks will be skipped anyway.
+		# But only 3. still applies. If I want to make an ocean, I can remove the exemption.
 		for old_terrain_entity in old_terrain_entities when old_terrain_entity not instanceof Water
+			# TODO: prevent optimization if there's not many points
+			old_terrain_entity.intangible = true
+			old_terrain_entity.intangible_because_optimized = true
+
 			old_points = Object.values(old_terrain_entity.structure.points)
 			old_points_flat = []
 			for point in old_points
@@ -341,20 +363,25 @@ module.exports = class World
 						if Math.abs(polygon_coords[i] - cut_x) < epsilon
 							polygon_coords[i] += offset_dir * overlap
 
-			new_terrain_entities = []
 			for sliced_points_flat in polygons
 				sliced_points = []
 				for i in [0...sliced_points_flat.length] by 2
 					sliced_points.push({x: sliced_points_flat[i], y: sliced_points_flat[i+1]})
 				# Relying on the specific serialization format of PolygonStructure.
 				# It stores points as an array, unlike the base class Structure, which supports named points.
+				# Also, this doesn't need to be the original entity class,
+				# we could probably just use the base class Terrain.
+				# We shouldn't need to do anything special to make it invisible,
+				# since it's not going in the world.entities array.
 				ent_def = Object.assign(JSON.parse(JSON.stringify(old_terrain_entity)), {
 					structure: {points: sliced_points}
+					intangible: false
+					intangible_because_optimized: false
+					entity_collider_is_derived_from: old_terrain_entity
 				})
 				delete ent_def.id
 				new_terrain_entity = Entity.fromJSON(ent_def)
-				new_terrain_entities.push(new_terrain_entity)
-			@entities.splice(@entities.indexOf(old_terrain_entity), 1, ...new_terrain_entities)
+				@derived_colliders.push(new_terrain_entity)
 		return
 
 	collision: (point, {types=[Terrain], lineThickness=5}={})->
@@ -370,7 +397,7 @@ module.exports = class World
 			console.warn "Collision detection called before collision buckets were initialized."
 			@updateCollisionBuckets()
 
-		# no bucketization (to compare FPS, also disable updateCollisionBuckets)
+		# no bucketization (to compare FPS, also disable updateCollisionBuckets and perhaps optimizeTerrain)
 		# entities = @entities
 		# one dimensional bucketization
 		# b_x = Math.floor(point.x/bucket_width)
@@ -395,6 +422,8 @@ module.exports = class World
 			local_point = entity.fromWorld(point)
 			if entity.structure.pointInPolygon?
 				if entity.structure.pointInPolygon(local_point)
+					if entity.entity_collider_is_derived_from
+						return entity.entity_collider_is_derived_from
 					return entity
 			else
 				for segment_name, segment of entity.structure.segments
@@ -420,8 +449,6 @@ module.exports = class World
 
 	# This is used for collision response in Caterpillar and Arrow.
 	projectPointOutside: (point_in_world_space, {types=[Terrain], outsideEntity}={})->
-		# TODO: use non-split polygons for collision resolution
-		# Split polygons are good for collision detection, but not for collision response.
 		# Ideally it should use joined polygons, with a boolean polygon union operation,
 		# so that it projects to the overall boundary of the terrain, even across different types of terrain,
 		# and prevents potential strange responses at crevices between different polygons.

commit 12385ca80d54c7bfd1f3f292dc025cbe8700cb2b

Fix auto-spawn sometimes leaving entities at the bottom of the world
I found an unimportant regression, the auto-spawn dev feature sometimes leaving entities at the bottom of the world, and I was able to work around it, but I don't understand it yet.
Something to do with the new collision optimization system.
It was broken in ac5d8fe74c669af84ea2ab8a224e839b82e0bf65 "Fix collision response at seams by separating concerns"

src/main.coffee
@@ -238,10 +238,19 @@ do animate = ->
 	try
 		for class_name in class_names
 			min_instances = 10
-			if world.entities.filter((entity) -> entity.constructor.name is class_name).length < min_instances
+			existing_instances = world.entities.filter((entity) -> entity.constructor.name is class_name).length
+			if existing_instances < min_instances
 				ent = Entity.fromJSON({_class_: class_name})
 				ent.x = Math.random() * 1000
 				ent.y = bottom_of_world
+
+				# Fix auto-spawn sometimes leaving entities at the bottom of the world
+				# world.updateCollisionBuckets() # doesn't help
+				if not world.collision(ent)
+					console.log "Not auto-spawning #{class_name} (yet) because there's no ground at", ent.x, ent.y
+					console.log "Total entities:", world.entities.length, "Terrain entities:", world.entities.filter((entity) -> entity instanceof Terrain).length, "#{class_name} entities:", existing_instances
+					break
+
 				while world.collision(ent)
 					ent.y -= 3
 				world.entities.push(ent)